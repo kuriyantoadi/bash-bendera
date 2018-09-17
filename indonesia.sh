b43feb26679f08d875dc4b594205b381402cc902
kuriyantoadi <melatikuning212@gmail.com>
2018-09-17 15:29:14 +0700

    update 17 september 2018, update bendera

---
+++ indonesia.sh
@@ -0,0 +1,116 @@
+x=1
+while [ $x -le 58 ]
+do
+  echo -ne "\033[1;31m#\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;31mi\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;31m#\033[0m"
+
+echo ""
+echo -ne "\033[1;31m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;31mi\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;31m#\033[0m"
+
+echo ""
+echo -ne "\033[1;31m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;31mi\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;31m#\033[0m"
+echo ""
+
+echo -ne "\033[1;31m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;31mi\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;31m#\033[0m"
+echo ""
+
+echo -ne "\033[1;31m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;31mi\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;31m#\033[0m"
+echo ""
+
+########################################
+echo -ne "\033[1;37m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;37md\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;37m#\033[0m"
+
+echo ""
+echo -ne "\033[1;37m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;37md\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;37m#\033[0m"
+
+echo ""
+echo -ne "\033[1;37m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;37md\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;37m#\033[0m"
+echo ""
+
+echo -ne "\033[1;37m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;37md\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;37m#\033[0m"
+echo ""
+
+echo -ne "\033[1;37m#\033[0m"
+x=1
+while [ $x -le 56 ]
+do
+  echo -ne "\033[1;37md\033[0m"
+  x=$(( $x + 1 ))
+done
+echo -ne "\033[1;37m#\033[0m"
+echo ""
+
+x=1
+while [ $x -le 58 ]
+do
+  echo -ne "\033[1;37m#\033[0m"
+  x=$(( $x + 1 ))
+done
+echo ""
---
+++ palestina.sh
@@ -0,0 +1,138 @@
+#bendera palestina
+
+echo -ne "\033[1;31m 1\033[0m"
+#perulangan 58
+x=1
+while [ $x -le 58 ]
+do
+  echo -ne "\033[1;30mP\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 1111\033[0m"
+#perulangan 55
+x=1
+while [ $x -le 55 ]
+do
+  echo -ne "\033[1;30mP\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 11111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 51 ]
+do
+  echo -ne "\033[1;30mP\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 1111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 46 ]
+do
+  echo -ne "\033[1;30mP\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 11111111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 42 ]
+do
+  echo -ne "\033[1;37mS\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 111111111111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 38 ]
+do
+  echo -ne "\033[1;37mS\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+x=1
+while [ $x -le 25 ]
+do
+  echo -ne "\033[1;31m1\033[0m"
+  x=$(( $x + 1 ))
+done
+
+#perulangan 51
+x=1
+while [ $x -le 34 ]
+do
+  echo -ne "\033[1;37mS\033[0m"
+  x=$(( $x + 1 ))
+done
+
+
+echo ""
+echo -ne "\033[1;31m 111111111111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 38 ]
+do
+  echo -ne "\033[1;37mS\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 11111111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 42 ]
+do
+  echo -ne "\033[1;37mS\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 1111111111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 46 ]
+do
+  echo -ne "\033[1;32mE\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 11111111\033[0m"
+#perulangan 51
+x=1
+while [ $x -le 51 ]
+do
+  echo -ne "\033[1;32mE\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 1111\033[0m"
+#perulangan 55
+x=1
+while [ $x -le 55 ]
+do
+  echo -ne "\033[1;32mE\033[0m"
+  x=$(( $x + 1 ))
+done
+
+echo ""
+echo -ne "\033[1;31m 1\033[0m"
+#perulangan 58
+x=1
+while [ $x -le 58 ]
+do
+  echo -ne "\033[1;32mE\033[0m"
+  x=$(( $x + 1 ))
+done
+echo ""
